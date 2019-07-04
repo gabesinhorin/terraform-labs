06df9976d2e170d2a14c5748ec951789ee529256
Gabriel Sinhorin <gabriel.sinhorin@avanade.com>
2019-07-04 16:23:05 -0400

    a

--- nsgs.tf
+++ nsgs.tf
@@ -88,3 +88,22 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         destination_address_prefix = "*"
   }
 }
+
+resource "azure_network_security_group" "nic_windows" {
+    name = "NIC_Windows"
+    resource_group_name = "${azurerm_resource_group.nsgs.name}"
+    location = "${azurerm_resource_group.nsgs.location}"
+    tags = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name = "RDP"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "RDP"
+        source_port_range = "*"
+        destination_port_range = 3389
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
